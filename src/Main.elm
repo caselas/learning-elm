ff09bd3acb282f592600bbbf853b593a5d729f38
Chris Caselas <dev@chriscaselas.me>
2017-02-13 23:31:41 -0800

    Fetch player data from server.

--- src/Main.elm
+++ src/Main.elm
@@ -1,22 +1,26 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, text, program)
-import Messages exposing (Msg)
+import Messages exposing (Msg(..))
 import Models exposing (Model, initialModel)
+import Players.Commands exposing (fetchAll)
 import Update exposing (update)
 import View exposing (view)
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( initialModel, Cmd.none )
+    ( initialModel, Cmd.map PlayersMsg fetchAll )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
+
+
 -- MAIN
+
 
 main : Program Never Model Msg
 main =
